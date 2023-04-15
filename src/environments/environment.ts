2e4bdc5b5020014f2f8733c36f06ffc0865a644c
AboubakrNasef <nasefabobakr725@gmail.com>
2023-04-15 23:03:20 +0200

    Added routing functionality to the app

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+firebase:{
+    apiKey: "AIzaSyCYmSpk-MASzgWITNJVRM3hsI_FzWAO35Y",
+    authDomain: "clipsbeacon.firebaseapp.com",
+    projectId: "clipsbeacon",
+    storageBucket: "clipsbeacon.appspot.com",
+    messagingSenderId: "862966606284",
+    appId: "1:862966606284:web:2859a87bc2c537588d9569"
+  }
+};
